f8858c108a993ed2f37b9f66b4bfe177d4518884
Chrissy Wainwright <chrissy@sixfeetup.com>
2020-11-16 08:47:04 -0600

    updates to training type
    changed instructor field name to 'speaker', since this is already
    indexed, and is the same type of data
    
    display time zone and ical download on training view, and show speaker
    images

--- ploneconf2016/policy/content/training_class.py
+++ ploneconf2016/policy/content/training_class.py
@@ -31,6 +31,23 @@ class TrainingClassView(DefaultView):
                 instructor_data.append({'url':x.absolute_url(), 'name': x.title})
         return instructor_data
 
+    def speakers(self):
+        """ parse speaker set into something template can iterate over
+        """
+        speaker_set = self.context.speaker
+        speakers = [api.content.get(UID=x) for x in speaker_set]
+        speaker_data = []
+        for x in speakers:
+            scale_func = api.content.get_view(name='images', context=x, request=self.request)
+            # scale choices are taken from portal_registry/edit/plone.allowed_sizes
+            scaled_image = getattr(x.aq_explicit, 'headshot', False) and scale_func.scale('headshot', width=150, height=150)
+            if scaled_image:
+                tag = scaled_image.tag(css_class='image-left')
+            else:
+                tag = ''
+            speaker_data.append({'url': x.absolute_url(), 'name': x.title, 'headshot': tag})
+        return speaker_data
+
     def vocab_title(self, values, vocab):
         """ return title when given the vocabulary and the value key
         """
